27d6f3e2599a98801a2a132cb50d2fd5826362c5
David_Valdaliso_Lopez <dvaldaliso@gmail.com>
2023-04-01 19:01:50 -0400

    terminado

--- src/main/java/cu/uci/e18a/spark/WordCount.java
+++ src/main/java/cu/uci/e18a/spark/WordCount.java
@@ -1,7 +1,17 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package cu.uci.e18a.spark;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
 
 /**
  *
@@ -9,11 +19,36 @@ import org.apache.spark.sql.SparkSession;
  */
 public class WordCount {
     public static void main(String[] args) {
-        SparkSession spark = SparkSession.
-                builder()
+        SparkSession spark = SparkSession
+                .builder()
                 .appName("WordCount")
-                .master("local[*]").getOrCreate();
+                .master("local[*]")
+                .getOrCreate();
         spark.sparkContext().setLogLevel("ERROR");
         
+        JavaRDD<String> lines = spark.read().textFile("book.txt").javaRDD();
+        JavaRDD<String> words = lines.flatMap(s->Arrays.asList(s.split(" ")).iterator());
+        
+        //Eliminando caracteres raros
+        JavaRDD<String> preproc = words.map(s->{
+            String t = s.replaceAll("[^a-zA-ZáéíóúñÁÉÍÓÚÑ]","");
+            return t.toLowerCase();
+        });
+        
+        JavaPairRDD<String, Integer> ones = words.mapToPair(s->new Tuple2<>(s,1));
+        JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1,i2)->i1+i2);
+        List<Tuple2<String, Integer>> tuples = new ArrayList<>(counts.collect());
+        
+         tuples.sort((t1,t2)->{
+            Integer i1 = t1._2();
+            Integer i2 = t2._2();
+            return -1*i1.compareTo(i2);
+        });
+         
+        for(Tuple2<?,?> tuple : tuples){
+            System.out.println(tuple._1()+" "+tuple._2());
+        }        
+         spark.stop();
     }
+    
 }
